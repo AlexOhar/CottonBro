0715886e1ce2854c76569bc94f60dc07d2f929e1
AlexOhar <122322748+AlexOhar@users.noreply.github.com>
2023-01-10 02:00:39 +0300

    first add

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu_dropDown'),
+    menuItem = document.querySelectorAll('.list_item'),
+    hamburger = document.querySelector('.header_catalog');
+    cross = document.querySelector('.menu_dropDown_cross');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('menu_dropDown_active');
+    });
+
+    cross.addEventListener('click', () => {
+        menu.classList.toggle('menu_dropDown_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.toggle('menu_dropDown_active');
+        });
+    });
+});
